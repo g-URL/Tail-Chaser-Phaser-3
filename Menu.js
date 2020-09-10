375dc7cc41428b975f99169051ce8684fee11039
g-URL <rapperley@ucalgary.ca>
2020-09-10 14:45:15 -0600

    Refactored scenes into classes.

---
+++ Menu.js
@@ -0,0 +1,33 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super({key:"Menu"});
+    }
+
+
+    preload ()
+    {
+        // https://www.codeandweb.com/free-sprite-sheet-packer
+        this.load.atlas('board', 'assets/sprites/board.png', 'assets/sprites/board.json');
+        this.load.atlas('menu', 'assets/sprites/menu.png', 'assets/sprites/menu.json');
+        this.load.atlas('mother', 'assets/sprites/mother.png', 'assets/sprites/mother.json');
+    }
+
+    
+    create ()
+    {
+        this.add.image(320, 320, 'board', 'board.png');
+        this.add.image(320, 320 - 120, 'menu', 'game_title.png');
+
+        // https://www.html5gamedevs.com/topic/36850-solvederror-thisaddbutton-is-not-a-function/
+        this.newGameButton = this.add.image(320, 320 + 100, 'menu', 'new_game_0.png').setInteractive();
+        this.newGameButton.on('pointerover', function() { this.setFrame('new_game_1.png'); });
+        this.newGameButton.on('pointerout', function() { this.setFrame('new_game_0.png'); });
+        //this.newGAmeButton.on('pointerdown', startGame);
+
+        var logo = this.physics.add.image(400, 100, 'mother', 'mother_south_0.png');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+    }
+} 
--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-/// <reference path="ts/phaser.d.ts" />
+var config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 640,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: [ Menu ]
+};
 
-
+var game = new Phaser.Game(config);
