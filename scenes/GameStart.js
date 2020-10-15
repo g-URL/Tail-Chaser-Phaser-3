5fcb99268dcfdcb7f2fa288642b5e7f77110775e
g-URL <rapperley@ucalgary.ca>
2020-10-15 16:23:12 -0600

    Kittens following is working and a collision with a clowder member resulsts in game over.

--- scenes/GameStart.js
+++ scenes/GameStart.js
@@ -130,13 +130,14 @@ class GameStart extends Phaser.Scene {
                                     this.yarn, 
                                     this.foodBowl], 
                                     this);
+
     }
 
     update(time, delta)
     {
         // if the mother collides with any obstacles (or her clowder - to be added later) then game over
         this.physics.collide(this.mother, this.obstacles, function() { this.scene.start('GameOver'); }, null, this);
-        //this.physics.collide(this.mother, this.clowder, function() { this.scene.start('GameOver'); }, null, this);
+        this.physics.collide(this.mother, this.clowder, function() { this.scene.start('GameOver'); }, null, this);
 
         console.log(this.physics.collide(this.kittens, this.obstacles, null, null, this));
         console.log(this.kittens.countActive());
@@ -237,33 +238,66 @@ class GameStart extends Phaser.Scene {
 
             if (this.cat.direction == 'mother_north') {
                 if (this.cat.leader.direction == 'mother_north') {
-                    this.cat.y = this.cat.leader.y + 33;
-                } else if (Phaser.Math.Difference(this.cat.x, this.cat.leader.x) > 32) {
-                    this.cat.y = this.cat.leader.y;
-                    this.cat.direction = this.cat.leader.direction;
+                    //this.cat.y = this.cat.leader.y + 33;
+                    this.cat.y--;
+                } else if (this.cat.leader.direction == 'mother_south'){
+                    this.scene.start('GameOver')
+                } else {
+                    if (Phaser.Math.Difference(this.cat.x, this.cat.leader.x) > 32) {
+                        this.cat.y = this.cat.leader.y;
+                        this.cat.direction = this.cat.leader.direction;
+                    } else {
+                        //this.cat.y = this.cat.leader.y + 33;
+                        this.cat.y--;
+                    }
                 }
+
             } else if (this.cat.direction == 'mother_south') {
                 if (this.cat.leader.direction == 'mother_south') {
-                    this.cat.y = this.cat.leader.y - 33;
-                } else if (Phaser.Math.Difference(this.cat.x, this.cat.leader.x) > 32) {
-                    this.cat.y = this.cat.leader.y;
-                    this.cat.direction = this.cat.leader.direction;
+                    //this.cat.y = this.cat.leader.y - 33;
+                    this.cat.y++;
+                } else if (this.cat.leader.direction == 'mother_north') {
+                    this.scene.start('GameOver')
+                } else {
+                    if (Phaser.Math.Difference(this.cat.x, this.cat.leader.x) > 32) {
+                        this.cat.y = this.cat.leader.y;
+                        this.cat.direction = this.cat.leader.direction;
+                    } else {
+                        //this.cat.y = this.cat.leader.y - 33;
+                        this.cat.y++;  
+                    }
                 }
             } else if (this.cat.direction == 'mother_east') {
                 if (this.cat.leader.direction == 'mother_east') {
-                    this.cat.x = this.cat.leader.x - 33;
-                } else if (Phaser.Math.Difference(this.cat.y, this.cat.leader.y) > 32) {
-                    this.cat.x = this.cat.leader.x;
-                    this.cat.direction = this.cat.leader.direction;
+                    //this.cat.x = this.cat.leader.x - 33;
+                    this.cat.x++;                    
+                } else if (this.cat.leader.direction == 'mother_west') {
+                    this.scene.start('GameOver')
+                } else {
+                    if (Phaser.Math.Difference(this.cat.y, this.cat.leader.y) > 32) {
+                        this.cat.x = this.cat.leader.x;
+                        this.cat.direction = this.cat.leader.direction;
+                    } else {
+                        //this.cat.x = this.cat.leader.x - 33;
+                        this.cat.x++;
+                    }
                 }
             }
             // must be west
             else {
                 if (this.cat.leader.direction == 'mother_west') {
-                    this.cat.x = this.cat.leader.x + 33;
-                } else if (Phaser.Math.Difference(this.cat.y, this.cat.leader.y) > 32) {
-                    this.cat.x = this.cat.leader.x;
-                    this.cat.direction = this.cat.leader.direction;
+                    //this.cat.x = this.cat.leader.x + 33;
+                    this.cat.x--;
+                } else if (this.cat.leader.direction == 'mother_east') {
+                    this.scene.start('GameOver')
+                } else {
+                    if (Phaser.Math.Difference(this.cat.y, this.cat.leader.y) > 32) {
+                        this.cat.x = this.cat.leader.x;
+                        this.cat.direction = this.cat.leader.direction;
+                    } else {
+                        //this.cat.x = this.cat.leader.x + 33;
+                        this.cat.x--;
+                    }
                 }
             }
 
@@ -298,10 +332,8 @@ class GameStart extends Phaser.Scene {
         for (this.i = 0; this.i < this.kittens.getLength(); this.i++) {
             this.kitten = this.kitten_array[this.i];
             if (this.physics.collide(this.kitten, this.mother, null, null, this)) {
-                this.clowder.add(this.kitten);
+
                 this.kittens.remove(this.kitten);
-
-
 
                 if (this.mother.tail == null) {
                     console.log("FIRST KITTEN");
@@ -315,6 +347,7 @@ class GameStart extends Phaser.Scene {
                     this.mother.tail.follower = this.kitten;
                     this.kitten.leader = this.mother.tail;
                     this.mother.tail = this.kitten;
+                    this.clowder.add(this.kitten);
                 }
 
                 this.kitten.direction = this.kitten.leader.direction;
