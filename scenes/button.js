8d7a55a3b42b6f55c43ddd641c325c7d033ea981
g-URL <rapperley@ucalgary.ca>
2020-11-12 19:56:49 -0700

    fixed button class

--- scenes/button.js
+++ scenes/button.js
@@ -2,68 +2,63 @@ const black = 0x000000;
 const white = 0xffffff;
 
 class Button extends Phaser.GameObjects.Rectangle {
-    constructor(scene, x, y, width, height, input = null, fillColor=white, fillAlpha=1) {
+    constructor(scene, x, y, width, height, input=null, fillColor=white, fillAlpha=1) {
         super(scene, x, y, width, height, fillColor, fillAlpha);
 
         this.input = input;
-        this.text;
+        this.scene = scene;
+
+        this.setWhite();
+
+        // https://snowbillr.github.io/blog/2018-07-03-buttons-in-phaser-3/
+        this.setInteractive()
+            .on('pointerover', function() { this.setBlack(); })
+            .on('pointerout', function() { this.setWhite(); });
+
 
         // lines 28-30: https://github.com/photonstorm/phaser3-examples/blob/master/public/src/physics/arcade/extending%20arcade%20sprite.js
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
+        this.scene.add.existing(this);
 
-        this.addText();
+        this.text = this.scene.add.text(this.x, this.y, this.input, {fontFamily: 'EightbyFive'}, this).setInteractive()
+            .on('pointerover', function() { this.setBlack(); }, this)
+            .on('pointerout', function() { this.setWhite(); }, this);
 
+        this.defaultText();
     }
 
-    addText() {
-        this.text = this.scene.add.text(this.x, 
-                                        this.y, 
-                                        this.input, 
-                                        {fontFamily: 'EightbyFive',
-                                         fontSize: this.height+'px',    // best guess based on height of button
-                                         fontStyle: 'bold',
-                                         color: 'black',
-                                        },
-                                        this,
-                                        );
+    defaultText() {
+        this.text.setColor('black');
+        this.text.setFontStyle('bold');
+
+        // best guess based on height of button
+        this.text.setFontSize(this.height+'px');
 
         // https://www.html5gamedevs.com/topic/40451-bubble-dialog-with-text-centered-container-phaser-3140/
         this.text.setOrigin(0.5);
-        this.text.setFontSize(64);
 
         // some elements of font are taller than others, best guess to center based on height of button
-        let size = this.text.style.fontSize;
-        size = size.substring(0, size.length-2);
-        this.text.setY(this.y - size/8);
-
-        //this.text.setY(this.y - this.text.style.fontSize/8);
-        console.log(size);
+        this.text.setY(this.y - Phaser.Math.FloorTo(this.height/8));
     }
 
-    setText(newText) {
-        this.text.setText(newText);
+    getText() {
+        return this.text;
     }
 
-    setTextSize(size) {
-        this.text.setSize(size);
-    }
-
-    setTextStyle(style) {
-        this.text.setStyle(style);
-    }
-
-    setTextColor(color) {
-        this.text.setColor(color);
+    centerText() {
+        // MAKE THIS LATER
     }
 
     setBlack() {
         this.setFillStyle(black, 1);
-        this.setStrokeStyle(1, white, 1);
+        this.setStrokeStyle(2, white, 1);
+
+        if (this.text) { this.text.setColor('white'); }
     }
 
     setWhite() {
         this.setFillStyle(white, 1);
-        this.setStrokeStyle(1, black, 1);
+        this.setStrokeStyle(2, black, 1);
+
+        if (this.text) { this.text.setColor('black'); }
     }
 }
--- scenes/settings.js
+++ scenes/settings.js
@@ -15,10 +15,15 @@ class Settings extends Phaser.Scene {
         let myText = this.add.text(200,200,'VEST vest', {fontFamily: 'EightbyFive', fontSize: '32px', fontStyle: 'italic', color: 'black'});
         //myText.style.setFontSize(32);
 
+        const test = this.add.rectangle(400, 400, 32, 32, 0xffffff, 1).setInteractive();
+        test.on('pointerover', function() { this.setFillStyle(0x000000,1) });
+        test.on('pointerout', function() { this.setFillStyle(0xffffff,1) });
+
+
         // https://www.html5gamedevs.com/topic/36850-solvederror-thisaddbutton-is-not-a-function/
-        let choice = new Button(this, 320, 320, 128, 32, 'TESTING');
-        //choice.on('pointerover', function() { choice.setBlack(); });
-        //choice.on('pointerout', function() { choice.setWhite(); });
+        this.choice = new Button(this, 320, 320, 400, 40, 'SPECIAL THANKS TO THOMAS');
+        //let text = this.choice.getText();
+        //text.setFontSize('40px');
 
         this.add.rectangle(200, 200, 128, 32, 0xffffff, 1);
     }
